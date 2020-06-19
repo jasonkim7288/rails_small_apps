b51ea643bb315c7eb22064bfbdbc46a829cf5f49
jasonkim7288 <jason.kim7288@gmail.com>
2020-06-19 20:15:21 +1000

    temp test for js- 2

---
+++ app/javascript/packs/pokemons.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'js-autocomplete/auto-complete.css';
+import autocomplete from 'js-autocomplete';
+
+$(document).on('turbolinks:load', function() {
+  // This is for pokemon search
+  const names_pokemon = JSON.parse(document.getElementById('search-data-pokemon').dataset.names)
+  const searchInputPokemon = document.getElementById('search_text_pokemon');
+
+  if (names_pokemon && searchInputPokemon) {
+    new autocomplete({
+      selector: searchInputPokemon,
+      minChars: 1,
+      source: function(term, suggest){
+          term = term.toLowerCase();
+          const choices = names_pokemon;
+          const matches = [];
+          for (let i = 0; i < choices.length; i++)
+              if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+          suggest(matches);
+      },
+    });
+  }
+});
